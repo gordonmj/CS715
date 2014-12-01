fe9a27490baff4e40ef7d558896df14b71545425
justin <rival95@hotmail.com>
2014-11-30 23:32:19 -0500

    options displayed to admin/user accts

--- CS715/src/rmi/client/Client.java
+++ CS715/src/rmi/client/Client.java
@@ -10,13 +10,102 @@ import rmi.tasks.Authenticate;
 
 public class Client implements Runnable {
 	private static final int	PORT		= 24690;
-	private static Scanner		mConsole	= new Scanner(System.in);
+	private static Scanner		mScanner	= new Scanner(System.in);
 
 	private User				mUser;
+	private boolean				mExit		= false;
 
 	@Override
 	public void run() {
 		login();
+
+		while (!mExit) {
+			displayOptions();
+			String option = getOption();
+			performOption(option);
+		}
+	}
+
+	private void performOption(String option) {
+		if (mUser.isAdminAcct()) {
+			switch (option) {
+			case "1":
+
+				break;
+			case "2":
+
+				break;
+			case "3":
+
+				break;
+			case "4":
+
+				break;
+			case "5":
+
+				break;
+			case "6":
+
+				break;
+			case "7":
+
+				break;
+			case "8":
+				mExit = true;
+				break;
+
+			default:
+				break;
+			}
+		} else {
+			switch (option) {
+			case "1":
+
+				break;
+			case "2":
+
+				break;
+			case "3":
+
+				break;
+			case "4":
+
+				break;
+			case "5":
+
+				break;
+			case "6":
+				mExit = true;
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	private String getOption() {
+		return mScanner.nextLine();
+	}
+
+	private void displayOptions() {
+		if (mUser.isAdminAcct()) {
+			System.out.println("(1) Create/Delete User Account (RMI item 2)");
+			System.out.println("(2) Reset Password for User Account (RMI item 3) (admin may change password for all accounts)");
+			System.out.println("(3) Display List of User Account (RMI item 4)");
+			System.out.println("(4) Display schedule associated with specific user (RMI item 5)");
+			System.out.println("(5) Add an event to selected user’s schedule (RMI item 6)");
+			System.out.println("(6) Edit an event from selected user’s schedule (RMI item 7)");
+			System.out.println("(7) Delete an event from selected user’s schedule (RMI item 8)");
+			System.out.println("(8) Exit Program");
+		} else {
+			System.out.println("(1) Display user’s schedule (RMI item 5)");
+			System.out.println("(2) Add an event to user’s schedule (RMI item 6)");
+			System.out.println("(3) Edit an event from user’s schedule (RMI item 7)");
+			System.out.println("(4) Delete an event from user’s schedule (RMI item 8)");
+			System.out.println("(5) Change password to user’s account (RMI item 3) (user may only change his/her password)");
+			System.out.println("(6) Exit Program");
+		}
 	}
 
 	/**
@@ -42,10 +131,10 @@ public class Client implements Runnable {
 		boolean status = false;
 
 		System.out.println("Enter user name");
-		String username = mConsole.nextLine();
+		String username = mScanner.nextLine();
 
 		System.out.println("Enter password");
-		String password = mConsole.nextLine();
+		String password = mScanner.nextLine();
 
 		try {
 			String name = "Event";
