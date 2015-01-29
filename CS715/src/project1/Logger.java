f5e3909ad758810eba20df7376566bf36fa52fa0
justin <rival95@hotmail.com>
2015-01-29 12:42:46 -0500

    changed time format when printing logs

--- CS715/src/project1/Logger.java
+++ CS715/src/project1/Logger.java
@@ -1,8 +1,9 @@
 package project1;
 
 public class Logger {
+	private static final long	start	= System.nanoTime();
 
 	public synchronized static void log(String comment) {
-		System.out.println(System.nanoTime() + " " + comment);
+		System.out.println("[" + (System.nanoTime() - start) + "] " + comment);
 	}
 }
